aa904cfcc7908904094e7dcab36183b5de1ed678
Abdallah <ba0127542@gmail.com>
2025-04-12 11:54:18 +0200

    longitudinal_final

--- Task 4/Code/Task_4_longtudinal.m
+++ Task 4/Code/Task_4_longtudinal.m
@@ -90,13 +90,15 @@ theta_dth_lp = Long_lp_TF(2,2);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Step response for elevator input
-
-
 elevator_inputs = deg2rad([1, 5, 25]); % different values for delta elevator 
-T_final = 800;
+T_final = 300;
 t = linspace(0, T_final, 1000);
 filename = 'figures\Longitudinal Results\response\delta_e';
 
+delta_aileron = 0;
+delta_rudder = 0;
+delta_elevator =0;
+delta_thrust = 0;
 
 for j = 1:length(elevator_inputs) 
     elevator_input = elevator_inputs(j);
@@ -120,17 +122,17 @@ for j = 1:length(elevator_inputs)
     %%%%% w
     subplot(2,1,1);
     plot(t, y_full(:,2)+w0, 'b', 'LineWidth', 1.5); hold on; 
-    plot(t, y_short(:,1)+w0, 'r-', 'LineWidth', 1.5); hold on;
+    plot(t, y_short(:,1)+w0, 'g-', 'LineWidth', 1.5); hold on;
    
-    plot(simData{3}.time, simData{3}.data, '--g', 'LineWidth', 1.5);
-    title('Step Response For 5 deg. \delta_e')
+    plot(simData{3}.time, simData{3}.data, '--r', 'LineWidth', 1.5);
+    
     ylabel('w (m/s)'); grid on;
     legend('linear Full Response', 'Short Period Approximation', 'Non Linear');
     %%%%% q
     subplot(2,1,2);
-    plot(t, rad2deg(y_full(:,3))+q0, 'b', 'LineWidth', 1.5); hold on;
-    plot(t,rad2deg(y_short(:,2))+q0, 'r-', 'LineWidth', 1.5); hold on;
-    plot(simData{5}.time, simData{5}.data, '--g', 'LineWidth', 1.5);
+    plot(t, rad2deg(y_full(:,3))+rad2deg(q0), 'b', 'LineWidth', 1.5); hold on;
+    plot(t,rad2deg(y_short(:,2))+rad2deg(q0), 'g-', 'LineWidth', 1.5); hold on;
+    plot(simData{5}.time, simData{5}.data, '--r', 'LineWidth', 1.5);
     ylabel('q (deg/s)'); xlabel('Time (seconds)'); grid on;
     legend('linear Full Response', 'Short Period Approximation', 'Non Linear');
 
@@ -146,15 +148,15 @@ for j = 1:length(elevator_inputs)
     %%%%% u
     subplot(2,1,1);
     plot(t, y_full(:,1)+u0, 'b', 'LineWidth', 1.5); hold on; 
-    plot(t, y_long(:,1)+u0, 'r-', 'LineWidth', 1.5); hold on;
-    plot(simData{1}.time, simData{1}.data, '--g', 'LineWidth', 1.5);
+    plot(t, y_long(:,1)+u0, 'g-', 'LineWidth', 1.5); hold on;
+    plot(simData{1}.time, simData{1}.data, '--r', 'LineWidth', 1.5);
     ylabel('u (m/s)'); grid on;
     legend('linear Full Response', 'long Period Approximation', 'Non Linear');
     %%%%% theta
     subplot(2,1,2);
-    plot(t, rad2deg(y_full(:,4))+theta0, 'b', 'LineWidth', 1.5); hold on; 
-    plot(t, rad2deg(y_long(:,2))+theta0, 'r-', 'LineWidth', 1.5); hold on;
-    plot(simData{8}.time, simData{8}.data, '--g', 'LineWidth', 1.5);
+    plot(t, rad2deg(y_full(:,4))+rad2deg(theta0), 'b', 'LineWidth', 1.5); hold on; 
+    plot(t, rad2deg(y_long(:,2))+rad2deg(theta0), 'g-', 'LineWidth', 1.5); hold on;
+    plot(simData{8}.time, simData{8}.data, '--r', 'LineWidth', 1.5);
     ylabel('{\theta} (deg)'); xlabel('Time (seconds)'); grid on;
     legend('linear Full Response', 'long Period Approximation','Non Linear');
     sgtitle(['Long Period Mode - \delta_e = ', num2str(rad2deg(elevator_input)), '°']);
@@ -165,9 +167,12 @@ for j = 1:length(elevator_inputs)
 end
 %% Step response for thrust input
 thrust_inputs = [2000, 10000]; % different values for delta thrust (ibs)
-T_final = 800;
 t = linspace(0, T_final, 1000);
 filename = 'figures\Longitudinal Results\response\delta_th';
+delta_aileron = 0;
+delta_rudder = 0;
+delta_elevator =0;
+delta_thrust = 0;
 for j = 1:length(thrust_inputs) 
     thrust_input = thrust_inputs(j);
     SS_Long_thrust_input = SS_Long(:,2); 
@@ -190,16 +195,16 @@ for j = 1:length(thrust_inputs)
     %%%%% w
     subplot(2,1,1);
     plot(t, y_full(:,2)+w0, 'b', 'LineWidth', 1.5); hold on; 
-    plot(t, y_short(:,1)+w0, 'r', 'LineWidth', 1.5);
+    plot(t, y_short(:,1)+w0, 'g', 'LineWidth', 1.5);
     hold on;   
-    plot(simData{3}.time, simData{3}.data, '--g', 'LineWidth', 1.5);
+    plot(simData{3}.time, simData{3}.data, '--r', 'LineWidth', 1.5);
     legend('linear Full Response', 'long Period Approximation','Non Linear');
     ylabel('w (m/s)'); grid on;
     %%%%% q
     subplot(2,1,2);
-    plot(t, rad2deg(y_full(:,3))+q0, 'b', 'LineWidth', 1.5); hold on; 
-    plot(t, rad2deg(y_short(:,2))+q0, 'r', 'LineWidth', 1.5);
-    plot(simData{5}.time, simData{5}.data, '--g', 'LineWidth', 1.5);
+    plot(t, rad2deg(y_full(:,3))+rad2deg(q0), 'b', 'LineWidth', 1.5); hold on; 
+    plot(t, rad2deg(y_short(:,2))+rad2deg(q0), 'g', 'LineWidth', 1.5);
+    plot(simData{5}.time, simData{5}.data, '--r', 'LineWidth', 1.5);
     ylabel('q (deg/s)'); xlabel('Time (seconds)'); grid on;
     legend('linear Full Response', 'Short Period Approximation', 'Non Linear');
     sgtitle(['Short Period Mode - \delta_T = ', num2str(thrust_input), ' lbs']);
@@ -213,15 +218,15 @@ for j = 1:length(thrust_inputs)
     figure('units','normalized','outerposition',[0 0 1 1])
     subplot(2,1,1);
     plot(t, y_full(:,1)+u0, 'b', 'LineWidth', 1.5); hold on; 
-    plot(t, y_long(:,1)+u0, 'r', 'LineWidth', 1.5);
-    plot(simData{1}.time, simData{1}.data, '--g', 'LineWidth', 1.5);
+    plot(t, y_long(:,1)+u0, 'g', 'LineWidth', 1.5);
+    plot(simData{1}.time, simData{1}.data, '--r', 'LineWidth', 1.5);
     ylabel('u (m/s)'); grid on;
     legend('linear Full Response', 'long Period Approximation', 'Non Linear');
     %%%%% theta
     subplot(2,1,2);
-    plot(t, rad2deg(y_full(:,4))+theta0, 'b', 'LineWidth', 1.5); hold on; 
-    plot(t, rad2deg(y_long(:,2))+theta0, 'r', 'LineWidth', 1.5);
-    plot(simData{8}.time, simData{8}.data, '--g', 'LineWidth', 1.5);
+    plot(t, rad2deg(y_full(:,4))+rad2deg(theta0), 'b', 'LineWidth', 1.5); hold on; 
+    plot(t, rad2deg(y_long(:,2))+rad2deg(theta0), 'g', 'LineWidth', 1.5);
+    plot(simData{8}.time, simData{8}.data, '--r', 'LineWidth', 1.5);
     ylabel('{\theta} (deg)'); xlabel('Time (seconds)'); grid on;
     legend('linear Full Response', 'long Period Approximation','Non Linear');
 
